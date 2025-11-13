39ac7011374d19ffe5c455358f1e929f646193bf
TheCodeLamp <12064217+TheCodeLamp@users.noreply.github.com>
2025-11-13 20:35:36 +0100

    Install through rpmfusion instead

--- build_files/install-megasync.sh
+++ build_files/install-megasync.sh
@@ -2,14 +2,7 @@
 
 set -ouex pipefail
 
-source /etc/os-release
-
-FILE="/tmp/megasync.rpm"
-curl -L -o $FILE "https://mega.nz/linux/repo/Fedora_$VERSION_ID/x86_64/megasync-Fedora_$VERSION_ID.x86_64.rpm"
-dnf install --assumeyes $FILE
-rm $FILE
-
-FILE="/tmp/megasync-dolphin.rpm"
-curl -L -o $FILE "https://mega.nz/linux/repo/Fedora_$VERSION_ID/x86_64/dolphin-megasync-Fedora_$VERSION_ID.x86_64.rpm"
-dnf install --assumeyes $FILE
-rm $FILE
+# https://github.com/ublue-os/main/blob/e55976282cf282b28a5c58e3754328e156aaf0c7/build_files/nvidia-install.sh#L18
+dnf config-manager setopt rpmfusion-nonfree.enabled=1
+dnf install -y megasync dolphin-megasync
+dnf config-manager setopt rpmfusion-nonfree.enabled=0
